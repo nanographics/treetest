9707dc8b2d300983c47af66e395112ed7ae15fac
Lucas Melo <lwcmelo@gmail.com>
2023-07-31 13:00:25 +0200

    Added tree placer

---
+++ loadplots/loadplots.js
@@ -0,0 +1,143 @@
+/*const COLUMN_MAP = {
+    Plot: "number",
+    BaumNr: "number",
+    Year: "year",
+    x_koord: "xCoord",
+    y_koord: "yCoord",
+    elevation: "elevation",
+    Wuchsgebiet: "growingArea",
+    BFI_Bezirksforstinspektion: "district",
+    PlotRepArea: "area",
+    TreeNr: "number",
+    Age: "age",
+    Diameter: "diameter",
+    Tree_height: "treeHeight",
+    Trunk_height: "trunkHeight",
+    Crown_height: "crownHeight",
+    Crown_diameter: "crownDiameter",
+    Snag: "snag",
+    Tree_species_LA: "speciesLatin",
+    Tree_species_DE: "speciesGerman",
+    Tree_species_EN: "speciesEnglish",
+    N_rep_ha: "countPerHectare",
+    Plot_tree: "uniqueId"
+};*/
+
+export class Tree {
+    number;
+    age;
+    diameter;
+    treeHeight;
+    trunkHeight;
+    crownHeight;
+    crownDiameter;
+    snag; // 0=living, 1=dead
+    speciesLatin;
+    speciesGerman;
+    speciesEnglish;
+    countPerHectare;
+    uniqueId;
+    constructor(number, age, diameter, treeHeight, trunkHeight, crownHeight, crownDiameter, snag, speciesLatin, speciesGerman, speciesEnglish, countPerHectare, uniqueId) {
+        this.number = number;
+        this.age = age;
+        this.diameter = diameter;
+        this.treeHeight = treeHeight;
+        this.trunkHeight = trunkHeight;
+        this.crownHeight = crownHeight;
+        this.crownDiameter = crownDiameter;
+        this.snag = snag;
+        this.speciesLatin = speciesLatin;
+        this.speciesGerman = speciesGerman;
+        this.speciesEnglish = speciesEnglish;
+        this.countPerHectare = countPerHectare;
+        this.uniqueId = uniqueId;
+    }
+}
+
+export class Plot {
+    number;
+    year;
+    xCoord;
+    yCoord;
+    elevation;
+    district;
+    area;
+    growingArea; // Wuchsgebiet
+    constructor(number, year, xCoord, yCoord, elevation, district, area, growingArea) {
+        this.number = number;
+        this.year = year;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.elevation = elevation;
+        this.district = district;
+        this.area = area;
+        this.growingArea = growingArea;
+    }
+
+    trees = [];
+
+    addTree(tree) {
+        this.trees.push(tree);
+    }
+}
+
+export class PlotsSet {
+    plotYears = new Map();
+    constructor(csvString) {
+        const lines = csvString.replace(/\r/g, "").split("\n");
+
+        const columns = lines[0].split(",").map(x => x.substring(1, x.length - 1));//.map(x => COLUMN_MAP[x]);
+
+        for (let i = 1; i < lines.length; i++) {
+            const row = new Map();
+
+            lines[i]
+                .split(",")
+                .map(x => x.startsWith("\"") ? x.replace(/"/g, "").trim() : parseFloat(x))
+                .forEach((x, i) => row.set(columns[i], x));
+
+            let plotYears = this.plotYears.get(row.get("Plot"));
+            if (plotYears === undefined) {
+                plotYears = new Map();
+                this.plotYears.set(row.get("Plot"), plotYears);
+            }
+            
+            let plot = plotYears.get(row.get("Year"));
+            if (plot === undefined) {
+                plot = new Plot(
+                    row.get("Plot"),
+                    row.get("Year"),
+                    row.get("x_koord"),
+                    row.get("y_koord"),
+                    row.get("elevation"), 
+                    row.get("BFI_Bezirksforstinspektion"),
+                    row.get("PlotRepArea"),
+                    row.get("Wuchsgebiet")
+                );
+                plotYears.set(plot.year, plot);
+            }
+
+            const tree = new Tree(
+                row.get("TreeNr"),
+                row.get("Age"),
+                row.get("Diameter"), 
+                row.get("Tree_height"),
+                row.get("Trunk_height"),
+                row.get("Crown_height"),
+                row.get("Crown_diameter"),
+                row.get("Snag"),
+                row.get("Tree_species_LA"),
+                row.get("Tree_species_DE"),
+                row.get("Tree_species_EN"),
+                row.get("N_rep_ha"),
+                row.get("Plot_tree")
+            );
+            
+            plot.addTree(tree);
+        }
+    }
+
+    getPlot(plotNumber, year) {
+
+    }
+}
---
+++ loadplots/treeplacer.js
@@ -0,0 +1,120 @@
+export class TreePlacer {
+    constructor(heightDataUin8, widthPixels, heightPixels, widthMeters, heightMeters) {
+        this.heightDataUin8 = heightDataUin8;
+        this.widthPixels = widthPixels;
+        this.heightPixels = heightPixels;
+        this.widthMeters = widthMeters;
+        this.heightMeters = heightMeters;
+
+        this.ux = 1 / this.widthPixels; // X unit
+        this.uy = 1 / this.heightPixels; // Y unit
+    }
+
+    // Gets a float value at the position (x,y) in the texture, with x and y in range [0,1]
+    float(x, y) {
+        x = Math.min(this.widthPixels - 1, Math.max(0, Math.floor(x * this.widthPixels - 0.5)));
+        y = Math.min(this.heightPixels - 1, Math.max(0, Math.floor(y * this.heightPixels - 0.5)));
+        const i = 4 * (x + this.widthPixels * y);
+        return new Float32Array(this.heightDataUin8.buffer, i, 1)[0]; // Convert to float and return
+    }
+    minFloat(x, y) {
+        return Math.min(
+            this.float(x-this.ux/2, y-this.ux/2),
+            this.float(x+this.ux/2, y-this.uy/2),
+            this.float(x-this.ux/2, y+this.uy/2),
+            this.float(x+this.ux/2, y+this.uy/2)
+        );
+    }
+
+    get areaInSquareMeters() {
+        return this.widthMeters * this.heightMeters;
+    }
+
+    get areInHectares() {
+        return this.areaInSquareMeters / 10000;
+    }
+
+    sampleHeightInMeters(xInMeters, yInMeters) {
+        return this.minFloat(xInMeters / this.widthMeters, yInMeters / this.heightMeters);
+    }
+
+    placeTreesForSingleYear(plotsSet, plotNumbers, year) {
+        console.assert(plotNumbers.length > 0, "No plots given");
+
+        if (plotNumbers.length > 1) {
+            console.warn("TreePlacer.placeTreesForSingleYear() is work in progress and will only use one plot for now.");
+        }
+
+        const positionsOutput = [];
+
+        const plot = plotsSet.plotYears.get(plotNumbers[0]).get(year);
+
+        console.log("Terrain area in hectares:", this.areInHectares);
+
+        for (const tree of plot.trees) {
+
+            const treeCount = Math.round(tree.countPerHectare * this.areInHectares);
+
+            console.log(tree.speciesEnglish, treeCount, tree);
+
+            for (let i = 0; i < treeCount; i++) {
+                    
+                    const x = Math.random();
+                    const z = Math.random();
+                    const y = this.sampleHeightInMeters(x, z);
+
+                    positionsOutput.push({
+                        x: x * this.widthMeters,
+                        y: y,
+                        z: z * this.heightMeters
+                    });
+
+            }
+
+        }
+
+        /*const trees = new Map();
+
+        for (const plotInput of plotsInput) {
+
+            const treesInput = plotInput.years.get(year);
+            for (const treeInput of treesInput) {
+
+                let treesOfSameSpecies = trees.get(treeInput.speciesLatin);
+                if (treesOfSameSpecies === undefined) {
+                    treesOfSameSpecies = [];
+                    trees.set(treeInput.speciesLatin, treesOfSameSpecies);
+                }
+                treesOfSameSpecies.push({ treeInput, plotInput });
+
+            }
+
+        }
+        
+        for (const [speciesLatin, treesOfSameSpecies] of trees) {
+            for (const tree of treesOfSameSpecies) {
+
+                const treeCount = Math.round(tree.plotInput.area / tree.treeInput.countPerHectare);
+
+                console.log(treeCount, tree.treeInput);
+
+                for (let i = 0; i < treeCount; i++) {
+                        
+                        const x = Math.random();
+                        const z = Math.random();
+                        const y = this.sampleHeightInMeters(x, z);
+    
+                        positionsOutput.push({
+                            x: x * this.widthMeters,
+                            y: y,
+                            z: z * this.heightMeters
+                        });
+
+                }
+
+            }
+        }*/
+
+        return positionsOutput;
+    }
+}
